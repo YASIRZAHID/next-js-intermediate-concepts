3b8001eacedf805fefb9b5b65a1b3651ee7fa514
YASIR ZAHID <science5661@gmail.com>
2023-11-20 01:59:09 +0500

    Add a loading skeleton

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,6 +6,7 @@ import { AiFillBug } from "react-icons/ai";
 import classnames from "classnames";
 import { useSession } from "next-auth/react";
 import { Avatar, Box, Container, DropdownMenu, Flex, Text } from "@radix-ui/themes";
+import {Skeleton} from "@/app/components";
 
 const NavBar = () => {
   
@@ -51,7 +52,7 @@ const NavLinks = () => {
 
 const AuthStatus = ()=>{
   const { status, data: session } = useSession();
-  if (status==='loading') return;
+  if (status==='loading') return <Skeleton width="3rem"/>;
   if (status==='unauthenticated') return <Link className="nav-link" href="/api/auth/signinxc">Log In </Link>;
 return (
   <Box>
